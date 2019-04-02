dd2a10cf81f28b901652cef9c69acd7eaa128bca
Stephane Couvreur <stephane.couvreur@feedzai.com>
2019-04-02 17:49:34 +0100

    [Chapter 2] [Session 1] Finished

--- Ch02/02_01/final/typeInference.scala
+++ Ch02/02_01/final/typeInference.scala
@@ -6,7 +6,7 @@ object typeInference {
   List(1, 2, 4.5)                                 //> res0: List[Double] = List(1.0, 2.0, 4.5)
   List(1, true)                                   //> res1: List[AnyVal] = List(1, true)
   List(1, true, "Peggy")                          //> res2: List[Any] = List(1, true, Peggy)
-  
+
   def addOne(x:Int) = x + 1                       //> addOne: (x: Int)Int
   def function1(x:Int) = if(x>0) 1 else "negative"//> function1: (x: Int)Any
 }
